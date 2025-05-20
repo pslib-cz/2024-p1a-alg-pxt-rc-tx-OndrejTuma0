23d73a25142648cbe6cdf236d6f1b477333d471d
Ondřej Tůma <ondrej.tuma.024@pslib.cz>
2025-05-20 08:41:40 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+radio.setGroup(45)
+radio.setFrequencyBand(56)
+let x: number
+let y: number
 
+basic.forever(function () {
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+
+    radio.sendString(`${x}, ${y}, ${control.deviceSerialNumber()}`)
+
+    basic.pause(20)
+})
