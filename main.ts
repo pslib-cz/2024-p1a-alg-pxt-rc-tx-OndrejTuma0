407145a5268b4bc04be366904105e646733d4eed
Ondřej Tůma <ondrej.tuma.024@pslib.cz>
2025-05-21 11:30:00 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ radio.setGroup(45)
 radio.setFrequencyBand(56)
 let x: number
 let y: number
+radio.setTransmitSerialNumber(true)
 
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
 
-    radio.sendString(`${x}, ${y})
+    radio.sendString(`${x}, ${y}`)
 
     basic.pause(20)
 })
